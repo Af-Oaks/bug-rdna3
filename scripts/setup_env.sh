15960a2f75a7513c4ccbd20f3e2d7c34e464d4bf
Af-Oaks <filho.freiretn@gmail.com>
2026-03-08 22:01:06 -0300

    fix: install packaging python module required by mesa 26

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -33,7 +33,7 @@ echo "[+] Setting up Python virtual environment for latest Meson..."
 python3 -m venv build/venv
 source build/venv/bin/activate
 pip install --upgrade pip
-pip install meson mako
+pip install meson mako packaging
 
 # 2. Build isolated RADV (Mesa 3D)
 echo "[+] Setting up isolated RADV (Mesa 3D)..."
